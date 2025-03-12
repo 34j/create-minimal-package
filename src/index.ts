50f988f0f9e988f0272ef8f212b226cff0f682f0
34j <34j.github@proton.me>
2025-03-12 19:45:12 +0900

    docs: wip

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,42 @@
 /**
- * > Zero-knowledge minimalistic template for creating a new npm package.
+ * Zero-knowledge ***minimalistic*** template for creating a new npm package.
  *
- * Inspired by [node-module-boilerplate](https://github.com/sindresorhus/node-module-boilerplate) and [typescript-npm-package-template](https://github.com/Atry/typescript-npm-package-template/tree/main), which is great but unnecessarily complex and poorly maintained.
+ * ---
+ *
+ * **Docs Demo**: [https://34j.github.io/npm-package-template/](https://34j.github.io/npm-package-template/)
+ *
+ * **Package Demo**: [https://www.npmjs.com/package/npm-package-template](https://www.npmjs.com/package/npm-package-template)
+ *
+ * ---
  *
  * ## Features
  *
- * The proof that the config is minimalistic is shown below. The main reason some lines of config are needed is because the project structure (`src`, `dist`, `test`) needs to be specified. We denote this reason `☆`.
+ * - The **proof** that the config is ***minimalistic*** is shown below.
+ * - The main reason some lines of config are needed is because the project structure (`src`, `dist`, `test`) needs to be specified. We denote this reason `☆`.
  *
- * - Github
- *   - [Semantic Release](https://github.com/semantic-release/semantic-release) config with [Commitizen](https://github.com/search?q=commitizen) support from [semantic-release を使って Node パッケージのリリースワークフローを自動化する](https://zenn.dev/wakamsha/articles/learn-semantic-release)
- *   - [Issue Templates](https://github.com/34j/npm-package-template/tree/main/.github/ISSUE_TEMPLATE) from [pypackage-template/.github/ISSUE_TEMPLATE at main · browniebroke/pypackage-template](https://github.com/browniebroke/pypackage-template/tree/main/.github/ISSUE_TEMPLATE)
- *   - [GitHub Actions](https://github.com/34j/npm-package-template/tree/main/.github/workflows) and [Codecov](https://about.codecov.io/) config is npm version of [pypackage-template/.github/workflows/ci.yml at main · browniebroke/pypackage-template](https://github.com/browniebroke/pypackage-template/blob/main/.github/workflows/ci.yml)
- * - [TypeScript](https://www.typescriptlang.org/) config inherits [@tsconfig/strictest](https://www.npmjs.com/package/@tsconfig/strictest) with minimal project-specific modifications (src/dist naming) [suggested by Fireship](https://www.npmjs.com/package/@tsconfig/strictest)
- * - Formatting
- *   - [Husky](https://github.com/typicode/husky) runs Lint Staged and typedoc
+ * - 👾Github
+ *   - [📦🚀semantic-release](https://github.com/semantic-release/semantic-release) config with [Commitizen](https://github.com/search?q=commitizen) support from [semantic-release を使って Node パッケージのリリースワークフローを自動化する](https://zenn.dev/wakamsha/articles/learn-semantic-release)
+ *   - [New Issue Templates](https://github.com/34j/npm-package-template/tree/main/.github/ISSUE_TEMPLATE) from [pypackage-template/.github/ISSUE_TEMPLATE at main · browniebroke/pypackage-template](https://github.com/browniebroke/pypackage-template/tree/main/.github/ISSUE_TEMPLATE)
+ *   - [💨GitHub Actions](https://github.com/34j/npm-package-template/tree/main/.github/workflows) and [Codecov](https://about.codecov.io/) config is npm version of [pypackage-template/.github/workflows/ci.yml at main · browniebroke/pypackage-template](https://github.com/browniebroke/pypackage-template/blob/main/.github/workflows/ci.yml)
+ * - ⚙[TypeScript](https://www.typescriptlang.org/) config inherits [@tsconfig/strictest](https://www.npmjs.com/package/@tsconfig/strictest) with minimal project-specific modifications (src/dist naming) [suggested by Fireship](https://www.npmjs.com/package/@tsconfig/strictest)
+ * - ✨️Formatting
+ *   - [🐶Husky](https://github.com/typicode/husky) runs Lint Staged and typedoc
  *   - [Lint Staged](https://github.com/okonet/lint-staged) just calls ESLint
  *   - [ESLint](https://eslint.org/) config just inherits [@antfu/eslint-config](https://www.npmjs.com/package/@antfu/eslint-config)
- * - Testing
- *   - [Jest](https://jestjs.io/) config is 4 lines, 2 lines for supporting Codecov and 2 lines for supporting Node.js
- * - Documentation
+ * - ⚠️ Testing
+ *   - [👢Jest](https://jestjs.io/) config is 4 lines, 2 lines for supporting Codecov and 2 lines for supporting Node.js
+ * - 📖Documentation
  *   - [GitHub Pages](https://pages.github.com/)
  *   - [TypeDoc](https://typedoc.org/) and  config is 4 lines, 1 line for automatic README.md generation and 2 lines for `☆`
  *   - [typedoc-plugin-markdown](https://github.com/tgreyuk/typedoc-plugin-markdown/tree/master/packages/typedoc-plugin-markdown) does not need config
- *   - [Renovate](https://docs.renovatebot.com/) config just inherits [config:best-practices](https://docs.renovatebot.com/presets-config/#configbest-practices)
+ *   - [🧱Renovate](https://docs.renovatebot.com/) config just inherits [config:best-practices](https://docs.renovatebot.com/presets-config/#configbest-practices)
  *
+ * <details>
+ * <summary>Removed Features (to achieve minimalism)</summary>
  * - Removed Commitizen integration and VS Code stuff (from [ryansonshine/typescript-npm-package-template](https://github.com/ryansonshine/typescript-npm-package-template)) because it's not maintained and complex. Commitizen can be still used (Semantic Release supports it natively).
  * - Removed Nix stuff (from [typescript-npm-package-template](https://github.com/Atry/typescript-npm-package-template/tree/main)) because it's not necessary.
  * - Removed Prettier due to @antfu/eslint-config's opinions.
+ * </details>
  *
  * ## Getting started
  *
@@ -43,9 +53,9 @@
  *   ```
  * 3. Add your npm token to your GitHub repository secrets as `NPM_TOKEN`.
  * 4. Enable the Codecov GitHub App [here](https://github.com/apps/codecov) and add `CODECOV_TOKEN` to your GitHub repository secrets.
- * 5. Remove everything from here and above
+ * 5. Remove everything above `---`.
  *
- * **This documentation is automatically generated by Typedoc from `src/index.ts`**
+ * > Inspired by [node-module-boilerplate](https://github.com/sindresorhus/node-module-boilerplate) and [typescript-npm-package-template](https://github.com/Atry/typescript-npm-package-template/tree/main), which is great but unnecessarily complex and poorly maintained.
  *
  * ---
  *
@@ -62,7 +72,10 @@
  * ---
  *
  * **Documentation**: [https://34j.github.io/npm-package-template/](https://34j.github.io/npm-package-template/)
+ *
  * **Source Code**: [https://github.com/34j/npm-package-template](https://github.com/34j/npm-package-template)
+ *
+ * **Package**: [https://www.npmjs.com/package/npm-package-template](https://www.npmjs.com/package/npm-package-template)
  *
  * ---
  *
@@ -101,6 +114,8 @@
  * ## API Reference
  *
  * @module
+ *
+ * **This documentation is automatically generated by Typedoc from `src/index.ts`.**
  */
 
 /**
