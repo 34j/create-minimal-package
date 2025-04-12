7d647f29ef1ff3d2a8371e8607c28d6321a3662c
34j <34j.github@proton.me>
2025-04-12 15:47:00 +0900

    feat: add submodule

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@
   @module
  */
 
+import { mySubmodule } from './submodule'
+
 /**
  * Lorem ipsum.
  */
 export const myPackage = (taco = ''): string => `${taco} from my package`
+
+export { mySubmodule }
